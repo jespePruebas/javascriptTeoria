98327bf90439b7efd0534329d4d1176ca03f6bad
jespePruebas <90710076+jespePruebas@users.noreply.github.com>
2022-09-06 22:33:20 +0000

    cambio

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-let c = console;
-let n="Jesus";
-c.log(n);
-c.log(n.toLocaleUpperCase());
-c.log("Perú".split("").reverse().join(""));
+let d = document;
+
+let id = d.getElementById("id");
+//let clases = d.getElementsByClassName("clase");
+
+id.style.color = "red";
+document.getElementsByClassName("clase").style.color = "blue";
+
+let c = d.getElementsByName("fname"); //<input name="fname" type="text" value="Doug"> 
+
+/*obligatorio lleva un indice*/
+//let list1= d.getElementsByTagName("LI")[0]; // <li>Coffee</li> 
+
+// The querySelector() method returns the first element that matches a specified CSS selector(s) in the d. 
+
+// Get the first <p> element in the d with class="example": 
+//let f = d.querySelector("p.example");
+//let e = (d.querySelector("#demo").innerHTML = "Hello World!");
+
+
+
+//let x = d.querySelectorAll(".example");
+
+//x[3].style.backgroundColor = "red";
+//button1.style.backgroundColor = "red";
+
+
+
+//let x1 = d.querySelectorAll("p"); //
+//x[3].style.backgroundColor = "red";
+
+
 
--- string.js
+++ string.js
@@ -3,7 +3,7 @@ let a = "Quispe Castro";
 let b =24
 let c = "HELLO, LOOK AT YOU!, Mr. Blue blue has a blue house";
 var d = new String("Quispe Castro");
-//cl.log(a.length); //25
+//cl.log(a.length); //Representa la longitud de una cadena
 //cl.log(a.constructor); //ƒ String() { [native code] }
 //cl.log(b.toString()); //24
 //var z = "Jesus \\ Quispe"; //Jesus \ Quispe.
@@ -32,46 +32,25 @@ var d = new String("Quispe Castro");
 /********************************************************************/
 //cl.log(a.replace("Castro", "Quispe")); //Quispe Quispe /reemplaza solo la primera coincidencia
 //cl.log(a.replace("CASTRO", "Quispe")); //ningun cambio
-//console.log(a.toUpperCase()); //QUISPE CASTRO
-//console.log(a.toLocaleUpperCase()); //QUISPE CASTRO / para otros idiomas
-//console.log(a.toLocaleLowerCase()); //quispe castro / para otros idiomas
-//console.log(a.toLowerCase()); //quispe castro
-//console.log(a.concat(" + ", b)); // Quispe Castro + 24
-//console.log(a.trim());//elimina los espacios en blanco en ambos extremos del string
-//console.log(a.charAt(1)); //u
-console.log(str.charCodeAt(6)); //64
-console.log(str.split("a")); //(3)["Me, Ll@mo, DAVID, C", "stro, C", "stro"]
-console.log(str.split("a", 2)); //(2) ["Me, Ll@mo, DAVID, C", "stro, C"]
-console.log(str.split(" ")); //(5) ["Me,", "Ll@mo,", "DAVID,", "Castro,", "Castro"]
-console.log(str.split(",")); //(5) ["Me", " Ll@mo", " DAVID", " Castro", " Castro"]
-console.log(str.split(",")[0]); //Me
-console.log(str.split(",")[1]); // Ll@mo
-console.log(str.split(",")[2]); // DAVID
-console.log(str.split(",", 3)); //(3) ["Me", " Ll@mo", " DAVID"]
-console.log(str.split(" ", 1)); //["Me,"]
-console.log(str1.split(/(\d)/)); //(5) ["         ", "1", "   Peruano    ", "1", "        "]
-console.log(str.split("").reverse().join("")); //ortsaC ,ortsaC ,DIVAD ,om@lL ,eM
-console.log(str.startsWith("Me")); //true
-console.log(str.startsWith("Me")); //true
-console.log(str.endsWith("tro")); //true
-console.log(str.endsWith("Ll@mo", 8)); //false
-console.log(str.endsWith("Ll@mo", 9)); //true
-console.log(String.fromCharCode(64)); //@
-console.log(str.includes("o,")); //true
-console.log(str.includes("DAVID", 18)); //false
+//cl.log(a.toUpperCase()); //QUISPE CASTRO
+//cl.log(a.toLocaleUpperCase()); //QUISPE CASTRO / para otros idiomas
+//cl.log(a.toLocaleLowerCase()); //quispe castro / para otros idiomas
+//cl.log(a.toLowerCase()); //quispe castro
+//cl.log(a.concat(" + ", b)); // Quispe Castro + 24
+//cl.log(a.trim());//elimina los espacios en blanco en ambos extremos del string
+//cl.log(a.charAt(1)); //u
+//cl.log(a.charCodeAt(1)); //117 / Alt +117 
 
-var str1 = "ab";
-var str2 = "cd";
-var str3 = "ef";
-var str4 = "ab";
-console.log(str1.localeCompare(str2)); //-1
-console.log(str3.localeCompare(str2)); //1
-console.log(str1.localeCompare(str4)); //0
-console.log(str1.repeat(2)); //abab
-
-var x = 100 / "Apple";
-var y = 20;
-console.log(isNaN(x)); //true
-console.log(isNaN(y)); //false
-
-document.write(num);
+/*El método split() divide un objeto de tipo String en un array (vector) de cadenas mediante la separación de la cadena en subcadenas.
+S: cadena.split([separador][,limite])
+*/
+//cl.log(c.split("u")); //(5) ["HELLO, LOOK AT YOU!, Mr. Bl", "e bl", "e has a bl", "e ho", "se"]
+//cl.log(c.split("u", 2)); // (2) ["HELLO, LOOK AT YOU!, Mr. Bl", "e bl"]
+//cl.log(c.split(" ")); //(11) ["HELLO,", "LOOK", "AT", "YOU!,", "Mr.", "Blue", "blue", "has", "a", "blue", "house"]
+//cl.log(c.split(",")); //(3) ["HELLO", " LOOK AT YOU!", " Mr. Blue blue has a blue house"]
+//cl.log(c.split(",")[0]); //HELLO
+//cl.log(c.split(",")[1]); // LOOK AT YOU!
+//cl.log(c.split(",")[2]); //  Mr. Blue blue has a blue house
+//cl.log(c.split(",", 3));// (3) ["HELLO", " LOOK AT YOU!", " Mr. Blue blue has a blue house"]
+//cl.log(c.includes("o,")); //false
+//cl.log(c.includes("house", 18)); //true
